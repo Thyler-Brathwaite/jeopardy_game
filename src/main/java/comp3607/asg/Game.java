08afb706c5d8f117c737c00df298dfd7c4c4ca3d
Thyler-Brathwaite <brathwaitethyler8@gmail.com>
2025-11-27 05:17:17 -0400

    full game implementation 2

--- src/main/java/comp3607/asg/Game.java
+++ src/main/java/comp3607/asg/Game.java
@@ -57,7 +57,6 @@ public class Game {
 
             TurnSummary summary = turnManager.PlayTurn(currentPlayer, board);
 
-<<<<<<< HEAD
             // Save turn summary for report
             report.addTurnSummary(summary);
 
@@ -69,12 +68,6 @@ public class Game {
             displayLeaderboard();
 
             // Next player
-=======
-         
-            eventLog.LogEvent("eventlog.csv", summary);
-
-        
->>>>>>> d24534048c945e2d4ba06da3376186cd41d1ddbe
             currentIndex = (currentIndex + 1) % players.size();
         }
 
